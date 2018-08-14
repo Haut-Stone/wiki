873299692f6857bf38da67523e708f4f91faee21
europecat <516560470@qq.com>
2018-08-14 23:14:53 +0800

    增加返回首页

--- src/components/BackTop/BackTop.vue
+++ src/components/BackTop/BackTop.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="backTop" @click="backTop" v-show="scrollTop > 500"></div>
+    <router-link to="/"><div class="backIndex"></div></router-link>
   </div>
 </template>
 
@@ -19,13 +20,18 @@ export default {
 
 <style scoped lang="scss">
 .backTop,.backIndex{
-    width: rem(48);
-    height: rem(48);
-    position: fixed;
-    right: rem(15);
-    bottom: rem(15);
-    background: url("../../assets/images/backTop.png") no-repeat;
-    background-size: rem(48);
-    z-index: 10;
+  width: rem(48);
+  height: rem(48);
+  position: fixed;
+  right: rem(15);
+  bottom: rem(15);
+  background: url("../../assets/images/backTop.png") no-repeat;
+  background-size: rem(48);
+  z-index: 10;
+}
+.backIndex{
+  bottom: rem(68);
+  background: url("../../assets/images/backHome.png") no-repeat;
+  background-size: rem(48);
 }
 </style>
