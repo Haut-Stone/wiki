f9ce3e0c811d0543a40d2e51758db2f74e54512c
SJH <haut.1604.stone@gmail.com>
2018-08-17 21:45:30 +0800

    添加了材料页面的css，

--- src/views/Item/item.vue
+++ src/views/Item/item.vue
@@ -6,29 +6,70 @@
       <h2>材料页</h2>
     </header>
     <section>
-      <router-view></router-view>
+      <div class="search-bar">
+        <input type="text" class="search-text">
+        <div class="line-fix"></div>
+        <button class="search-btn"></button>
+      </div>
+      <div class="filter">
+        <div class="block">
+          <h2>分类</h2>
+          <div class="arrow"></div>
+        </div>
+        <div class="block">
+          <h2>星级</h2>
+          <div class="arrow"></div>
+        </div>
+      </div>
     </section>
+    <transition name="fade">
+      <BackTop :scrollTop="scrollTop"></BackTop>
+    </transition>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
+import BackTop from "@/components/BackTop/BackTop";
+import router from "@/router";
+import { urlParse } from "@/assets/js/util";
+import Star from "@/components/Star/Star";
 export default {
   name: 'Item',
   data () {
     return {
-      msg: '我是材料菜单'
+      msg: '我是材料菜单',
+      scrollTop: 0,
     }
   },
   methods: {
     goHome () {
       this.$router.push('/home')
+    },
+    _handleScroll() {
+      let scrollTop = 
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      this.scrollTop = scrollTop;
     }
+  },
+  components: {
+    BackTop
+  },
+  mounted() {
+    window.addEventListener("scroll", this._handleScroll);
+    router.push({path: "/item/list/all/all"})
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .item {
+  height: rem(8000);
+  background-color: rgb(243, 243, 243);
   /* 顶部标题 */
   header {
       height: rem(44);
@@ -51,5 +92,75 @@ export default {
         background-size: rem(13);
     }
   }
+
+  // 搜索工具
+  .search-bar {
+    height: rem(42);
+    background-color: white;
+    position: relative;
+    padding: rem(10);
+    text-align: center;
+    border-bottom: 1px solid rgb(206, 206, 206);
+    .search-text {
+      border-radius: 6px;
+      border: 1px solid rgb(204, 204, 204);
+      background-color: rgb(243, 243, 243);
+      height: rem(30);
+      width: rem(270);
+      line-height: rem(30);
+    }
+    .line-fix {
+      position: absolute;
+      height: rem(32);
+      top: rem(11);
+      left: rem(330);
+      width: rem(0);
+      border-left: 1px solid rgb(206, 206, 206);
+    }
+    .search-btn {
+      position: absolute;
+      background-image: url(../../assets/images/search_icon.png);
+      background-size: rem(30);
+      background-color: transparent;
+      border: none;
+      height: rem(30);
+      width: rem(30);
+      left: rem(340);
+      top: rem(10);
+    }
+  }
+
+  // 过滤工具
+  .filter {
+    height: rem(42);
+    border-bottom: 1px solid rgb(206, 206, 206);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    h2 {
+      flex: 1;
+      color: rgb(10, 21, 36);
+    }
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.move-enter-active,
+.move-leave-active {
+  transition: all 0.5s;
+}
+.move-enter,
+.move-leave-to {
+  transform: translate(-6rem, 0);
+  opacity: 0;
 }
 </style>
--- src/views/Item/itemList.vue
+++ src/views/Item/itemList.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="item_list">
-    <h1>{{msg}}</h1>
+    <div class="wrapper"> 
+      <div class="block" v-for="(item, index) in itemList" :key="index">
+        <div class="icon"></div>
+        <div class="star"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,12 +14,148 @@ export default {
   name: 'ItemList',
   data () {
     return {
-      msg: '我是材料列表'
+      msg: '我是材料列表',
+      itemList: [
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        },
+        {
+          "id": 5023,
+          "name": "雷霆意志",
+          "icon": "后台相对应的图片的地址",
+          "star": 3,
+          "type": "曜日材料"
+        }
+      ],
+    }
+  },
+  methods: {
+    _initList() {
+      this.$http.get(this.HOST + "", {
+        before(request) {
+          if (this.previouseRequest) {
+            this.previouseRequest.abrot();
+          }
+          this.previouseRequest = request;
+        }
+      })
+      .then(response => {
+        let itemList = JSON.parse(response.data);
+        this.itemList = Object.assign({}, this.itemList, itemList)
+      })
+      .catch(error => {
+        console.log(error);
+        console.log("error 已经被显示了");
+        this.$router.push({path: "/404"});
+      })
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.item_list {
+  .wrapper {
+    margin-top: rem(40);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background-color: rgb(243, 243, 243);
+    .block {
+      margin-left: rem(10);
+      margin-right: rem(10);
+      margin-bottom: rem(10);
+      height: rem(64);
+      width: rem(64);
+      background-image: url(../../assets/images/blue_bg.png);
+      background-size: rem(64);
+      position: relative;
+      .icon {
+        height: rem(40);
+        width: rem(40);
+        position: absolute;
+        background-image: url(../../assets/images/turbo.png);
+        background-size: rem(40);
+        top: rem(10);
+        left: rem(10);
+      }
+      .star {
+        height: rem(10);
+        width: rem(30);
+        position: absolute;
+        background-image: url(../../assets/images/star_4.png);
+        background-size: rem(30);
+        top: rem(52);
+        left: rem(20);
+      }
+    }
+  }
+}
 </style>
